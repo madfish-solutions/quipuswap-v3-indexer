49439d2c1413bd726c3657c453025f59a6c164b5
Korney Vasilchenko <starkkor@gmail.com>
2023-02-15 10:43:11 +0000

    Add basic models; Change default db config

--- quipuswap-v3-indexer/models.py
+++ quipuswap-v3-indexer/models.py
@@ -2,6 +2,36 @@ from tortoise import fields
 from dipdup.models import Model
 
 
-class User(Model):
-    address = fields.CharField(36, pk=True)
-    balance = fields.IntField()
+class Pool(Model):
+    class Meta:
+        table = 'pool'
+
+    address = fields.CharField(max_length=42, pk=True)
+    tokens = fields.ManyToManyField('models.Token', through='pool_token')
+
+class Token(Model):
+    class Meta:
+        table = 'token'
+
+    id = fields.IntField(pk=True)
+    address = fields.TextField()
+    token_id = fields.TextField(null=True)
+    name = fields.TextField(null=True)
+    symbol = fields.TextField(null=True)
+    decimals = fields.IntField()
+
+class Swap(Model):
+    class Meta:
+        table = 'swap'
+        unique_together = ('id', 'timestamp')
+
+    id = fields.IntField(pk=True, allows_generated=True)
+    pool = fields.ForeignKeyField('models.Pool')
+    from_token = fields.ForeignKeyField('models.Token', "from_token_swaps")
+    to_token = fields.ForeignKeyField('models.Token', "to_token_swaps")
+    hash = fields.CharField(64)
+    amount_in = fields.DecimalField(100, 0)
+    amount_out = fields.DecimalField(100, 0)
+    sender = fields.CharField(42)
+    receiver = fields.CharField(42)
+    timestamp = fields.DatetimeField()
